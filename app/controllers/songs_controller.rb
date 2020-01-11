51c58617bf00dbf4751adbe3c806fc98a80394a7
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-10 21:34:31 -0800

    modifed my controllers because I think they were wrong and modifed the views folders and added a file to theartists views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,43 +1,38 @@
 class SongsController < ApplicationController
+    before_action :set_song, only: [:show, :destroy]
+
     def index
-        @songs = Song.all
+      @songs = Song.all
     end
-
+  
+    def show
+    end
+  
     def new
-        @song = Song.new
+      @song = Song.new
     end
-
+  
     def create
-        @song = Song.new(song_params)
-        if @song.save
-          redirect_to(songs_path)
-        else
-          render(:new)
-        end
-      end
-
-      def edit
-        @song = Song.find(params[:id])
-      end
-
-      def update
-        @song = Song.find(params[:id])
-        if @song.update(song_params)
-            redirect_to(songs_path)
-        else
-            render(:edit)
-        end
-      end
-
-      def destroy
-        @song = Song.find(params[:id])
-        @song.destroy
-        redirect_to(songs_path)
-      end
-
-      private
-
-      def song_params
-        params.require(:song).permit(:body)
+      @song = Song.create(song_params)
+  
+      if @song.save
+        redirect_to @song
+      else
+        render :new
       end
     end
+  
+    def destroy
+      @song.destroy
+      redirect_to(songs_path)
+    end
+  
+    private
+      def set_song
+        @song = Song.find(params[:id])
+      end
+  
+      def song_params
+        params.require(:song).permit(:title :released_on :genre :rating)
+      end
+  end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ApplicationRecord
     belongs_to :artist
     has_and_belongs_to_many :billboards
+    def full_song
+        "#{self.title} #{self.released_on} #{self.genre} #{self.rating}"
+    end
 end

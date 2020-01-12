2bd551982aa47d8ee625bfa22085773c817dee82
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-11 16:46:29 -0800

    modified and added files to songs folder in views. Have an error when trying to add billboards and in add songs it will not add to song

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
   resources :artists
-  resources :songs do
-    resources :billboards, only: [:index, :new, :create, :destroy]
-  end
+  resources :songs
+  resources :billboards
 end

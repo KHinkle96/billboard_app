5a643263ae32a4578f7f4475065d2a3661c787d8
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-17 17:31:10 -0800

    added admin column to users, made the user name artist@test.com an admin using the rails console so they can view the admin page. If not an admin nothing will happen when you try to access the page.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -33,9 +33,9 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
+  
+  config.authorize_with do
+    redirect_to main_app.root_path unless warden.user.admin == true
+   end
 end

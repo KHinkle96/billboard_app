b5801019e48376c2c718e657601593ca537a59da
khinkle96 <khinkle0512@gmail.com>
2020-03-16 19:58:29 -0700

    first commit

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
+require 'rspec/rails'
+require 'devise'
+
+RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, type: :controller
+  end
 
 RSpec.describe ArtistsController, type: :controller do 
     describe "new" do
         it "sets a new artist object" do
-            get :new
-            expect(assigns(:user)).to be_a Artist 
+            get :newz
+            expect(assigns(:artist)).to be_a Artist 
         end
     end
+end
